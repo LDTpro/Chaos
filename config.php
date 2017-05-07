ba947a85bdc2ec99f70aac442d3876558de5d7ca
Vir <mrvir@protonmail.com>
2017-05-08 01:07:14 +0700

    add instruction to config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-$DBHOST = "";
-$DBUSER = ""; 
-$DBNAME = "";
-$DBPW =  "";
-$ACCESS_TOKEN = "";
-$VERIFY_TOKEN = "";
+$DBHOST = ""; // Set your database host here
+$DBUSER = ""; // Set your database user here
+$DBNAME = ""; // Set your database name here
+$DBPW =  ""; // Set your database password here
+$ACCESS_TOKEN = ""; // Set your Facebook Page Access Token here
+$VERIFY_TOKEN = ""; // Set your secret token here for verify webhook
 
 ?>
